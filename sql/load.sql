8391d272d210c5c497c644347b6428cc54682e67
raymondlin1 <raymondlin1@github.com>
2019-04-29 08:21:02 -0700

    task 0

--- sql/load.sql
+++ sql/load.sql
@@ -1,3 +1,3 @@
-\COPY project1.artist(artist_id, artist_name) FROM '/home/cs143/data/artist.csv' WITH CSV;
-\COPY project1.song(song_id, artist_id, song_name, url_suffix) FROM '/home/cs143/data/song.csv' WITH CSV;
-\COPY project1.token(song_id, word, word_freq) FROM '/home/cs143/data/token.csv' WITH CSV;
+\COPY project1.artist(artist_id, artist_name) FROM '/home/cs143/data/artist.csv' DELIMITER ',' QUOTE '"' CSV;
+\COPY project1.song(song_id, artist_id, song_name, page_link) FROM '/home/cs143/data/song.csv' DELIMITER ',' QUOTE '"'  CSV;
+\COPY project1.token(song_id, token, count) FROM '/home/cs143/data/token.csv' DELIMITER ',' QUOTE '"' CSV;
--- sql/schema.sql
+++ sql/schema.sql
@@ -2,33 +2,30 @@ DROP SCHEMA IF EXISTS project1 CASCADE;
 CREATE SCHEMA project1;
 
 CREATE TABLE IF NOT EXISTS project1.artist (
-        artist_id                       smallint,
-        artist_name                     varchar(45) NOT NULL,
+        artist_id                       integer,
+        artist_name                     varchar(255),
         PRIMARY KEY (artist_id)
 );
 
 CREATE TABLE IF NOT EXISTS project1.song (
         song_id                         integer,
-        artist_id                       smallint NOT NULL,
-        song_name                       varchar(80) NOT NULL,
-        url_suffix                      varchar(110) NOT NULL,
+        artist_id                       integer,
+        song_name                       varchar(255),
+        page_link                       varchar(1000),
         PRIMARY KEY (song_id),
         FOREIGN KEY (artist_id) REFERENCES project1.artist(artist_id)
 );
 
 CREATE TABLE IF NOT EXISTS project1.token (
         song_id                         integer,
-        word                            varchar(110),
-        word_freq                       smallint NOT NULL,
-        PRIMARY KEY (song_id, word),
-        FOREIGN KEY (song_id) REFERENCES project1.song(song_id)
+        token                           varchar(255),
+        count	                        integer,
+        PRIMARY KEY (song_id, token)
 );
 
 CREATE TABLE IF NOT EXISTS project1.tfidf (
         song_id                         integer,
-        word                            varchar(110),
-        word_tfidf                      double precision NOT NULL,
-        PRIMARY KEY (song_id, word),
-        FOREIGN KEY (song_id, word) REFERENCES project1.token(song_id, word),
-        FOREIGN KEY (song_id) REFERENCES project1.song(song_id)
+        token                           varchar(255),
+        score	                        float,
+        PRIMARY KEY (song_id, token)
 );
